8151ef1a41f805685432b24369bda27ae120f57d
documentedpub <134017305+documentedpub@users.noreply.github.com>
2025-02-21 18:23:51 +0200

    Update and rename README.md to project-1.cs

---
+++ project-1.cs
@@ -0,0 +1,11 @@
+using System;
+namespace MyNamesspace
+{
+    public class MyApp
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hello to This NameSpace");
+        }
+    }
+}
